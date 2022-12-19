774fadbf8034be3ae66ebbdaa124dc057b261843
tamaina <tamaina@hotmail.co.jp>
2022-12-20 01:46:09 +0900

    Vite HMR (#1)
    * hmr
    
    * fix

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import Fastify from 'fastify';
 import fastifyStatic from '@fastify/static';
 import fastifyView from '@fastify/view';
+import fastifyProxy from '@fastify/http-proxy';
 import pug from 'pug';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
@@ -22,19 +23,35 @@ fastify.register(fastifyView, {
   },
 });
 
-fastify.register(fastifyStatic, {
-  root: join(_dirname, 'built'),
-  prefix: '/assets/',
-  decorateReply: false,
-});
-
-fastify.get('*', async (request, reply) => {
-  const clientManifest = JSON.parse(readFileSync('./built/manifest.json', 'utf-8'));
-  const clientEntry = clientManifest['src/main.ts'];
-
-  return reply.view('index', {
-    clientEntry,
+if (process.env.NODE_ENV !== 'production') {
+  fastify.register(fastifyProxy, {
+    upstream: 'http://127.0.0.1:5173',
+    prefix: '/assets',
+    rewritePrefix: '/assets',
   });
-});
+  fastify.get('*', async (request, reply) => {
+    return reply.view('index', {
+      clientEntry: {
+        file: 'src/main.ts',
+      },
+    });
+  });
+
+} else {
+  fastify.register(fastifyStatic, {
+    root: join(_dirname, 'built'),
+    prefix: '/assets/',
+    decorateReply: false,
+  });
+
+  fastify.get('*', async (request, reply) => {
+    const clientManifest = JSON.parse(readFileSync('./built/manifest.json', 'utf-8'));
+    const clientEntry = clientManifest['src/main.ts'];
+
+    return reply.view('index', {
+      clientEntry,
+    });
+  });
+}
 
 fastify.listen({ port: 3000, host: '0.0.0.0' });
